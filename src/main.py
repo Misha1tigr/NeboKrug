e8dfa2121da2511148998582d8edfbb4baa4afbe
Misha1tigr <Misha1tigr@gmail.com>
2024-05-15 16:23:50 +0300

    Added location search, save and load

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from settings_manager import save_settings, load_settings
+from settings_manager import save_settings, load_settings, save_locations
+from geocoding import search_location
 
 
 def create_menu_bar(root):
@@ -11,7 +12,7 @@ def create_menu_bar(root):
     menubar.add_command(label="Units of Measurement", command=lambda: settings_units_window(root))
 
     # Your Locations
-    menubar.add_command(label="Your Locations", command=lambda: open_sample_window("Your Locations"))
+    menubar.add_command(label="Your Locations", command=lambda: settings_locations_window(root))
 
     # Other Settings
     menubar.add_command(label="Other Settings", command=lambda: open_sample_window("Other Settings"))
@@ -40,14 +41,13 @@ def open_sample_window(title):
 
 def settings_units_window(root):
     def on_save_and_close():
-        settings = {
+        changed_settings = {
             "temperature_unit": temperature_unit_selected_option.get(),
             "wind_speed_unit": windspeed_unit_selected_option.get(),
             "precipitation_unit": precipitation_unit_selected_option.get(),
         }
-        save_settings(settings)  # Save the settings to a JSON file
+        save_settings(changed_settings)  # Save the settings to a JSON file
         settings_window.destroy()
-        root.attributes("-disabled", 0)  # Re-enable the main window
 
     settings = load_settings()  # Load settings from the JSON file
 
@@ -91,6 +91,95 @@ def settings_units_window(root):
 
     settings_window.transient(root)
     settings_window.grab_set()
+
+
+def settings_locations_window(root):
+    def on_search():
+        query = search_var.get()
+        if query:
+            results = search_location(query)
+            search_results_listbox.delete(0, tk.END)
+            for result in results:
+                name = result.get('name')
+                country = result.get('country')
+                admin1 = result.get('admin1', 'N/A')
+                latitude = result.get('latitude')
+                longitude = result.get('longitude')
+                full_name = f"{name}, {admin1}, {country} (Lat: {latitude}, Lon: {longitude})"
+                search_results_listbox.insert(tk.END, full_name)
+                search_results_listbox.results[full_name] = result
+
+    def on_add_location():
+        selected = search_results_listbox.curselection()
+        if selected:
+            location = search_results_listbox.get(selected)
+            stored_locations_listbox.insert(tk.END, location)
+            stored_locations_listbox.locations.append(search_results_listbox.results[location])
+
+    def on_delete_location():
+        selected = stored_locations_listbox.curselection()
+        if selected:
+            index = selected[0]
+            del stored_locations_listbox.locations[index]
+            stored_locations_listbox.delete(index)
+
+    def on_save_and_close():
+        locations = stored_locations_listbox.locations
+        save_locations(locations)  # Save the locations to the JSON file
+        locations_window.destroy()
+
+    locations = load_settings().get("locations", [])  # Load locations from the JSON file
+
+    locations_window = tk.Toplevel()
+    locations_window.title("Your Locations")
+    locations_window.protocol("WM_DELETE_WINDOW", on_save_and_close)
+
+    frame = ttk.Frame(locations_window, padding="10")
+    frame.pack(fill='both', expand=True)
+    ttk.Label(frame, text="Your saved locations").grid(column=0, row=0, columnspan=2, padx=5, pady=5)
+    # Listbox for stored locations
+    ttk.Label(frame, text="Stored Locations").grid(column=0, row=1, columnspan=2, padx=5, pady=5)
+    stored_locations_listbox = tk.Listbox(frame, height=10, width=80, exportselection=False)
+    stored_locations_listbox.locations = []
+    for location in locations:
+        name = location.get('name')
+        country = location.get('country')
+        admin1 = location.get('admin1', 'N/A')
+        latitude = location.get('latitude')
+        longitude = location.get('longitude')
+        full_name = f"{name}, {admin1}, {country} (Lat: {latitude}, Lon: {longitude})"
+        stored_locations_listbox.insert(tk.END, full_name)
+        stored_locations_listbox.locations.append(location)
+    stored_locations_listbox.grid(column=0, row=2, columnspan=2, padx=5, pady=5)
+
+    # Control buttons for locations
+    add_location_btn = ttk.Button(frame, text="Add", command=on_add_location)
+    add_location_btn.grid(column=0, row=3, padx=5, pady=5)
+
+    delete_location_btn = ttk.Button(frame, text="Delete", command=on_delete_location)
+    delete_location_btn.grid(column=1, row=3, padx=5, pady=5)
+
+    # Search field for locations
+    ttk.Label(frame, text="Search Location").grid(column=0, row=4, columnspan=2, padx=5, pady=5)
+    search_var = tk.StringVar()
+    search_entry = ttk.Entry(frame, width=50, textvariable=search_var)
+    search_entry.grid(column=0, row=5, columnspan=2, pady=5)
+
+    search_btn = ttk.Button(frame, text="Search", command=on_search)
+    search_btn.grid(column=0, row=6, columnspan=2, padx=5, pady=5)
+
+    # Listbox for search results
+    ttk.Label(frame, text="Search Results").grid(column=0, row=7, columnspan=2, padx=5, pady=5)
+    search_results_listbox = tk.Listbox(frame, height=10, width=80, exportselection=False)
+    search_results_listbox.results = {}
+    search_results_listbox.grid(column=0, row=8, columnspan=2, padx=5, pady=5)
+
+    # Save and Close Button
+    save_and_close_btn = ttk.Button(frame, text="Save & Close", command=on_save_and_close)
+    save_and_close_btn.grid(column=0, row=9, columnspan=2, pady=10)
+
+    locations_window.transient(root)
+    locations_window.grab_set()
 
 
 def create_tabs(root):
--- src/settings_manager.py
+++ src/settings_manager.py
@@ -7,11 +7,27 @@ settings_schema = {
     "type": "object",
     "properties": {
         "temperature_unit": {"type": "string", "enum": ["Celsius °C", "Fahrenheit °F"]},
-        "wind_speed_unit": {"type": "string", "enum": ["Km/h", "m/s", "Mph", "Knots"]},
+        "wind_speed_unit": {"type": "string", "enum": ["Km/h", "m/s", "mph", "Knots"]},
         "precipitation_unit": {"type": "string", "enum": ["Millimeter", "Inch"]},
+        "locations": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "name": {"type": "string"},
+                    "country": {"type": "string"},
+                    "latitude": {"type": "number"},
+                    "longitude": {"type": "number"},
+                },
+                "required": ["name", "country", "latitude", "longitude"]
+            }
+        }
     },
-    "required": ["temperature_unit", "wind_speed_unit", "precipitation_unit"],
+    "required": ["temperature_unit", "wind_speed_unit", "precipitation_unit"]
 }
+
+settings_loaded = False
+settings = {}
 
 
 def get_settings_path():
@@ -21,25 +37,41 @@ def get_settings_path():
 
 
 def load_settings():
-    settings_path = get_settings_path()
-    if os.path.exists(settings_path):
-        with open(settings_path, "r") as file:
-            settings = json.load(file)
-            validate_settings(settings)
-            print(settings)  #TODO remove
-            return settings
-    return {}
+    global settings_loaded, settings
+    if not settings_loaded:
+        settings_path = get_settings_path()
+        if os.path.exists(settings_path):
+            with open(settings_path, "r") as file:
+                settings = json.load(file)
+                validate_settings(settings)
+                print(settings)  #TODO remove
+                settings_loaded = True
+                return settings
+    else:
+        return settings
 
 
-def save_settings(settings):
-    validate_settings(settings)
-    settings_path = get_settings_path()
-    with open(settings_path, "w") as file:
-        json.dump(settings, file)
-
-
-def validate_settings(settings):
+def save_settings(new_settings):
+    global settings
     try:
-        validate(instance=settings, schema=settings_schema)
+        validate_settings(new_settings)
+        settings_path = get_settings_path()
+        with open(settings_path, "w") as file:
+            json.dump(new_settings, file)
+    except Exception as e:
+        print(f"Error saving settings: {e}")
+    settings = new_settings
+
+
+def validate_settings(settings_for_validation):
+    try:
+        validate(instance=settings_for_validation, schema=settings_schema)
     except ValidationError as e:
         raise ValueError(f"Invalid settings: {e.message}")
+
+
+
+def save_locations(locations):
+    settings_to_update = load_settings()
+    settings_to_update["locations"] = locations
+    save_settings(settings_to_update)
