0dd879e80851f6e93b06d9b3e303f20a82b5dbd3
Misha1tigr <Misha1tigr@gmail.com>
2024-05-18 17:17:54 +0300

    Added comments

--- src/settings_manager.py
+++ src/settings_manager.py
@@ -3,6 +3,7 @@ import platformdirs
 import os
 from jsonschema import validate, ValidationError
 
+# Schema for validating the settings JSON structure
 settings_schema = {
     "type": "object",
     "properties": {
@@ -26,17 +27,30 @@ settings_schema = {
     "required": ["temperature_unit", "wind_speed_unit", "precipitation_unit"]
 }
 
+# Global variables to hold the settings state
 settings_loaded = False
 settings = {}
 
 
 def get_settings_path():
+    """
+    Retrieve the path to the settings file.
+
+    Returns:
+        str: The full path to the settings.json file.
+    """
     settings_dir = platformdirs.user_config_dir("NeboKrug", "Korbut Mykhailo")
     os.makedirs(settings_dir, exist_ok=True)
     return os.path.join(settings_dir, "settings.json")
 
 
 def load_settings():
+    """
+    Load the settings from the settings.json file if it exists.
+
+    Returns:
+        dict: The loaded settings.
+    """
     global settings_loaded, settings
     if not settings_loaded:
         settings_path = get_settings_path()
@@ -51,6 +65,12 @@ def load_settings():
 
 
 def save_settings(new_settings):
+    """
+    Save the provided settings to the settings.json file.
+
+    Args:
+        new_settings (dict): The new settings to save.
+    """
     global settings
     try:
         validate_settings(new_settings)
@@ -63,20 +83,40 @@ def save_settings(new_settings):
 
 
 def validate_settings(settings_for_validation):
+    """
+    Validate the settings against the predefined schema.
+
+    Args:
+        settings_for_validation (dict): The settings to validate.
+
+    Raises:
+        ValueError: If the settings do not conform to the schema.
+    """
     try:
         validate(instance=settings_for_validation, schema=settings_schema)
     except ValidationError as e:
         raise ValueError(f"Invalid settings: {e.message}")
 
 
-
 def save_locations(locations):
+    """
+    Save the provided locations to the settings.
+
+    Args:
+        locations (list): The list of locations to save.
+    """
     settings_to_update = load_settings()
     settings_to_update["locations"] = locations
     save_settings(settings_to_update)
 
 
 def extract_units():
+    """
+    Extract units for temperature, wind speed, and precipitation from the settings.
+
+    Returns:
+        tuple: A tuple containing temperature unit, wind speed unit, and precipitation unit.
+    """
     settings_to_extract_from = load_settings()
     temperature_unit = settings_to_extract_from.get("temperature_unit")
     wind_speed_unit = settings_to_extract_from.get("wind_speed_unit")
